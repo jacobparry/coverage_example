b8f005dec7bec5d92d7a81d6dad1cb30a7065d3a
Jacob Parry <jacobparry@Jacobs-MacBook-Pro.local>
2024-04-24 13:17:54 -0600

    test fix

--- apps/cover_one/test/cover_one_test.exs
+++ apps/cover_one/test/cover_one_test.exs
@@ -2,25 +2,14 @@ defmodule CoverOneTest do
   use ExUnit.Case
   doctest CoverOne
 
-  describe "covered" do
-    test "covered_1" do
-      assert CoverOne.covered_1() == :covered
-    end
-
-    test "covered_2" do
-      assert CoverOne.covered_2() == :covered
-    end
-
-    test "covered_3" do
-      assert CoverOne.covered_3() == :covered
-    end
-
-    test "covered_4" do
-      assert CoverOne.covered_4() == :covered
-    end
-
-    test "covered_5" do
-      assert CoverOne.covered_5() == :covered
-    end
+  test "covered" do
+    assert CoverOne.covered_1() == :covered
+    assert CoverOne.covered_2() == :covered
+    assert CoverOne.covered_3() == :covered
+    assert CoverOne.covered_4() == :covered
+    assert CoverOne.covered_5() == :covered
+    assert CoverOne.covered_6() == :covered
+    assert CoverOne.covered_7() == :covered
+    assert CoverOne.covered_8() == :covered
   end
 end
--- apps/cover_two/test/cover_two_test.exs
+++ apps/cover_two/test/cover_two_test.exs
@@ -2,9 +2,7 @@ defmodule CoverTwoTest do
   use ExUnit.Case
   doctest CoverTwo
 
-  describe "covered" do
-    test "covered_1" do
-      assert CoverTwo.covered_1() == :covered
-    end
+  test "covered" do
+    assert CoverTwo.covered_1() == :covered
   end
 end
